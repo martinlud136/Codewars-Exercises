5fb977f222878ed95eae8c9806ec92a6d853da89
Martinlud <hectormartinluduena@gmail.com>
2020-04-26 19:11:15 -0300

    definir la relacion entre los primeros dos digitos

--- morseCode2.js
+++ morseCode2.js
@@ -1,8 +1,11 @@
-var decodeBits = function(bits){
+
+let decodeBits = function(bits){
     // ToDo: Accept 0's and 1's, return dots, dashes and spaces
     //1-eliminar ceros de los extremos
+    //Determinar cual es la menor unidad para punto y espacio
+    //parece que se define en la relacion entre los primeros dos caracteres
     //leer secuencia de caracteres y reemplazar:
-    //···· · −·−−   ·−−− ··− −·· ·
+    //".... . -.--   .--- ..- -.. ."
     //11--->punto de letra
     //111111--->barra de letra
     //00--->espacio entre puntos y barra en la letra    
@@ -21,31 +24,31 @@ var decodeBits = function(bits){
     let string = [];
 
     arr.reduce((valAnterior,valActual)=>{
-        let a = "1" ; b = "0";
+        //aca tengo que definir la relacion
         if(valActual === "1"){
             total1+= 1;
             switch(total0){
-                case 2:
+                case 1:
                     string.push("");
                     total0 = 0;
                 break;
-                case 6:
+                case 3:
                     string.push(" ");
                     total0 = 0;
                 break;
-                case 14:
+                case 7:
                     string.push("   ");
                     total0 = 0;
                 break;
             }
         }else{
             switch(total1){
-                case 2:
-                    string.push("·");
+                case 1:
+                    string.push(".");
                     total1 = 0;
                 break;
-                case 6:
-                    string.push("−");
+                case 3:
+                    string.push("-");
                     total1 = 0;
                 break;
             }
@@ -55,12 +58,12 @@ var decodeBits = function(bits){
         return 
     },"1")
     switch(total1){
-        case 2:
-            string.push("·");
+        case 1:
+            string.push(".");
             total1 = 0;
         break;
-        case 6:
-            string.push("−");
+        case 3:
+            string.push("-");
             total1 = 0;
         break;
     }
@@ -68,12 +71,58 @@ var decodeBits = function(bits){
     return string.join('')
 }
 
-var decodeMorse = function(morseCode){
+let morse= {
+    ".-": "A",
+    "-...": "B",
+    "-.-.": "C",
+    "-..": "D",
+    ".": "E",
+    "..-.": "F",
+    "--.": "G",
+    "....": "H",
+    "..": "I",
+    ".---": "J",
+    "-.-": "K",
+    ".-..": "L",
+    "--": "M",
+    "-.": "N",
+    "---": "O",
+    "−−−": "O",
+    ".--.": "P",
+    "--.-": "Q",
+    ".-.": "R",
+    "...": "S",
+    "···": "S",
+    "-": "T",
+    "..-": "U",
+    "...-": "V",
+    ".--": "W",
+    "-..-": "X",
+    "-.--": "Y",
+    "--..": "Z",
+    "...---...": "SOS",
+    "-.-.--": "!",
+    ".-.-.-": "."
 }
 
-let bits = '1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011';
+decodeMorse = function(morseCode){
+    let palabras = (morseCode.trim()).split("   ")
+    let decodificado = []
+    for(let i of palabras){
+      let letras = i.split(" ")
+        let palabra = [];
+        for(let a of letras){
+            let letra = morse[a]
+            palabra.push(letra)
+        }
+        decodificado.push(palabra.join(""))
+    }
+    return decodificado.join(" ")
+}
 
+let bits = '10111';
 
 console.log(decodeBits(bits))
+console.log(decodeMorse(decodeBits(bits)))
 
 //console.log(decodeBits('1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011'))
